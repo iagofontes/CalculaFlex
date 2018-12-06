2c11aebb8fd539305cb01e1a870440892f66571a
iagofontes <iagofontes@hotmail.com>
2018-12-05 22:54:46 -0200

    login no firebase, resta salvar usuário

--- app/src/main/java/br/com/iagofontes/calculaflex/SignUpActivity.kt
+++ app/src/main/java/br/com/iagofontes/calculaflex/SignUpActivity.kt
@@ -1,12 +1,67 @@
 package br.com.iagofontes.calculaflex
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import br.com.iagofontes.calculaflex.extension.getValue
+import br.com.iagofontes.calculaflex.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        btCreate.setOnClickListener {
+            FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(
+                            inputEmail.getValue(),
+                            inputPassword.getValue()
+                    ).addOnCompleteListener {
+                        if(it.isSuccessful) {
+                            saveInDatabase()
+                        } else {
+                            Toast.makeText(
+                                    this@SignUpActivity,
+                                    it.exception?.message,
+                                    Toast.LENGTH_SHORT).show()
+                        }
+                    }
+        }
+
     }
+
+    private fun saveInDatabase() {
+
+        val user = User(inputName.getValue(), inputEmail.getValue(), inputPhone.getValue())
+
+        FirebaseDatabase.getInstance()
+                .getReference("Users")
+                .child(FirebaseAuth.getInstance().currentUser!!.uid)
+                .setValue(user)
+                .addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        Toast.makeText(
+                                this@SignUpActivity,
+                                "Usuário criado com sucesso.",
+                                Toast.LENGTH_SHORT).show()
+                        val intent = Intent()
+                        intent.putExtra("EMAIL", inputEmail.getValue())
+                        setResult(Activity.RESULT_OK, intent)
+                        finish()
+                    } else {
+                        Toast.makeText(
+                                this@SignUpActivity,
+                                it.exception?.message,
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+    }
+
 }
